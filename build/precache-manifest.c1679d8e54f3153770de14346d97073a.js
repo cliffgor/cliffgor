df8f23306e70021934053fcdf0fa54bf6e21db9b
cliffgor <clierozigy@gmail.com>
2020-04-09 23:27:28 +0300

    first commit

---
+++ build/precache-manifest.c1679d8e54f3153770de14346d97073a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1eb5d687a7702347c87ebcd34256fe8",
+    "url": "/cliffgor.github.io/index.html"
+  },
+  {
+    "revision": "fcc3db26618d45115a66",
+    "url": "/cliffgor.github.io/static/css/main.87564d34.chunk.css"
+  },
+  {
+    "revision": "237b051cadd26eeeccc9",
+    "url": "/cliffgor.github.io/static/js/2.dfedf3e6.chunk.js"
+  },
+  {
+    "revision": "fcc3db26618d45115a66",
+    "url": "/cliffgor.github.io/static/js/main.add413b0.chunk.js"
+  },
+  {
+    "revision": "6457dcb3785e5187b2bd",
+    "url": "/cliffgor.github.io/static/js/runtime~main.ece7db0f.js"
+  }
+]);
