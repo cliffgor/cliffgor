fefb6dd33d7c34f8eef776755cecbeeb54ecb1c0
cliffgor <clierozigy@gmail.com>
2020-04-09 23:29:01 +0300

    Updates

---
+++ precache-manifest.99f8402bf92293d6e736fb92296dc5ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817d4bc85f063b792af08ae3bc131739",
+    "url": "/cliffgor/index.html"
+  },
+  {
+    "revision": "114d6228046d3dbe3498",
+    "url": "/cliffgor/static/css/main.87564d34.chunk.css"
+  },
+  {
+    "revision": "237b051cadd26eeeccc9",
+    "url": "/cliffgor/static/js/2.dfedf3e6.chunk.js"
+  },
+  {
+    "revision": "114d6228046d3dbe3498",
+    "url": "/cliffgor/static/js/main.8bec1257.chunk.js"
+  },
+  {
+    "revision": "937ad0b7ab3a847d7f57",
+    "url": "/cliffgor/static/js/runtime~main.184bdf87.js"
+  }
+]);
